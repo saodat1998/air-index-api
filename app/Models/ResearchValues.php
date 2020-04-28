545f8c1697608ddc409a9c0549615d8f7a6f526a
Sadirbaeva Saodat <saodat_9808@mail.ru>
2020-04-28 23:21:11 +0500

    static value

--- app/Models/ResearchValues.php
+++ app/Models/ResearchValues.php
@@ -16,9 +16,9 @@ class ResearchValues extends Model implements Transformable
 {
     protected $table = 'researcher';
 
-    protected $appends = ['qualities', 'technical_value', 'date', 'region_name'];
+    protected $appends = ['qualities', 'technical_value', 'date', 'region_name', 'statistic_value'];
 
-    protected $visible = ['id', 'technical_value', 'date', 'value', 'qualities', 'status', 'region_name'];
+    protected $visible = ['id', 'technical_value', 'date', 'value', 'qualities', 'status', 'region_name', 'statistic_value'];
 
     use TransformableTrait, PresentableTrait;
 
@@ -34,7 +34,18 @@ class ResearchValues extends Model implements Transformable
         'status',
         'date_id',
 	];
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function statisticValueModel()
+    {
+        return $this->hasOne(StatisticValues::class, 'research_value_id', 'id');
+    }
 
+    public function getStatisticValueAttribute()
+    {
+        return $this->statisticValueModel && $this->statisticValueModel->value ? $this->statisticValueModel->value : '';
+    }
     /**
      * @return \Illuminate\Database\Eloquent\Relations\HasOne
      */
