b50572b073a193fab451c172865c0c0a722be3a5
Sadirbaeva Saodat <saodat_9808@mail.ru>
2020-04-28 22:14:55 +0500

    Researcher values list

--- app/Models/ResearchValues.php
+++ app/Models/ResearchValues.php
@@ -16,6 +16,10 @@ class ResearchValues extends Model implements Transformable
 {
     protected $table = 'researcher';
 
+    protected $appends = ['qualities', 'technical_value', 'date'];
+
+    protected $visible = ['id', 'technical_value', 'date', 'value', 'qualities'];
+
     use TransformableTrait, PresentableTrait;
 
     /**
@@ -30,5 +34,45 @@ class ResearchValues extends Model implements Transformable
         'status',
         'date_id',
 	];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function technicalModel()
+    {
+        return $this->hasOne(TechnicalValues::class, 'id', 'technical_value_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function dateModel()
+    {
+        return $this->hasOne(Date::class, 'id', 'date_id');
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getQualitiesAttribute()
+    {
+        return $this->technicalModel && $this->technicalModel->qualities ? $this->technicalModel->qualities : '';
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTechnicalValueAttribute()
+    {
+        return $this->technicalModel && $this->technicalModel->value ? $this->technicalModel->value : '';
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateAttribute()
+    {
+        return $this->dateModel ? $this->dateModel->date : '';
+    }
 
 }
